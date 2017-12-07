c7336ad41529aec3eb7868c6fdb809b97f651b2f
Steve Gutz <sgutz@hubspot.com>
2017-12-07 14:28:10 -0500

    Add httpql-jersey1

--- httpql-jersey1/src/main/java/com/hubspot/httpql/jersey1/BindQuery.java
+++ httpql-jersey1/src/main/java/com/hubspot/httpql/jersey1/BindQuery.java
@@ -1,4 +1,4 @@
-package com.hubspot.httpql.jersey;
+package com.hubspot.httpql.jersey1;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,10 +7,6 @@ import java.lang.annotation.Target;
 
 import com.hubspot.httpql.QuerySpec;
 
-/**
- * @deprecated Use {@code com.hubspot.httpql.jersey1.BindQuery} from httpql-jersey1 instead.
- */
-@Deprecated
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface BindQuery {
