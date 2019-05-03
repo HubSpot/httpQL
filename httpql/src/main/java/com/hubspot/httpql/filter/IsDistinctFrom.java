14352e7ea3fc6919f3b141ea46f8a35e8f64ad97
Mohamed Gueye <mgueye@hubspot.com>
2019-05-03 15:56:52 -0400

    Add isDistinctFrom

---
+++ httpql/src/main/java/com/hubspot/httpql/filter/IsDistinctFrom.java
@@ -0,0 +1,35 @@
+package com.hubspot.httpql.filter;
+
+import java.util.Collection;
+
+import org.jooq.Condition;
+import org.jooq.Field;
+
+import com.hubspot.httpql.ConditionProvider;
+import com.hubspot.httpql.Filter;
+import com.hubspot.httpql.MultiParamConditionProvider;
+
+public class IsDistinctFrom extends FilterBase implements Filter {
+
+  @Override
+  public String[] names() {
+    return new String[] {
+        "distinct"
+    };
+  }
+
+  @Override
+  public <T> ConditionProvider<T> getConditionProvider(final Field<T> field) {
+    return new MultiParamConditionProvider<T>(field) {
+
+      @Override
+      public Condition getCondition(Collection<T> values) {
+        return values.stream()
+            .map(field::isDistinctFrom)
+            .reduce(Condition::and)
+            .orElseThrow(IllegalArgumentException::new);
+      }
+
+    };
+  }
+}
