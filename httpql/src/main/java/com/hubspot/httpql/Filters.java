26dd5ca70bb278f8b5830b1b5338769492082f49
Jeff Boulter <jboulter@hubspot.com>
2023-10-25 16:31:21 -0400

    add filter registry

---
+++ httpql/src/main/java/com/hubspot/httpql/Filters.java
@@ -0,0 +1,46 @@
+package com.hubspot.httpql;
+
+import com.hubspot.httpql.core.filter.FilterIF;
+import com.hubspot.httpql.impl.filter.FilterImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.ServiceLoader;
+
+public class Filters {
+
+    private static final Map<String, Filter> FILTERS_BY_NAME = new HashMap<>();
+    private static final Map<String, FilterImpl> FILTER_IMPLS_BY_NAME = new HashMap<>();
+    private static final ServiceLoader<Filter> FILTER_LOADER = ServiceLoader.load(Filter.class);
+    private static final ServiceLoader<FilterImpl> FILTER_IMPL_LOADER = ServiceLoader.load(FilterImpl.class);
+    private static final Map<Class<? extends FilterIF>, FilterImpl> FILTER_IMPLS = new HashMap<>();
+
+    static {
+        for (Filter filter : FILTER_LOADER) {
+            for (String name : filter.names()) {
+                FILTERS_BY_NAME.put(name, filter);
+            }
+        }
+
+        for (FilterImpl filter : FILTER_IMPL_LOADER) {
+            for (String name : filter.names()) {
+                FILTER_IMPLS_BY_NAME.put(name, filter);
+            }
+            FILTER_IMPLS.put(filter.getAnnotationClass(), filter);
+        }
+    }
+
+    public static Optional<FilterImpl> getFilterImplByName(String name) {
+        return Optional.ofNullable(FILTER_IMPLS_BY_NAME.get(name));
+    }
+
+    public static Optional<FilterImpl> getFilterImpl(Class<? extends FilterIF> filter) {
+        return Optional.ofNullable(FILTER_IMPLS.get(filter));
+    }
+
+    @Deprecated
+    public static Optional<Filter> getFilterByName(String name) {
+        return Optional.ofNullable(FILTERS_BY_NAME.get(name));
+    }
+}
