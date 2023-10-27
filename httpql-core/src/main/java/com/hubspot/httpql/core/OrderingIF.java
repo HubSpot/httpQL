33501043509d5d55380038bcc5ad174698c68c48
Jeff Boulter <jboulter@hubspot.com>
2023-10-27 09:16:41 -0400

    Revert "Revert "allow json deserialization from OrderingIF""
    This reverts commit 3a2e0d85e806c604fe0a127ddcacea645e1fe610.

--- httpql-core/src/main/java/com/hubspot/httpql/core/OrderingIF.java
+++ httpql-core/src/main/java/com/hubspot/httpql/core/OrderingIF.java
@@ -1,5 +1,8 @@
 package com.hubspot.httpql.core;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public interface OrderingIF {
 
     String getQueryName();
@@ -12,4 +15,20 @@ public interface OrderingIF {
      * @return either "asc" or "desc"
      */
     String getOrderString();
+
+    @JsonCreator
+    static Ordering fromString(String ordering) {
+        SortOrder order;
+        if (ordering.startsWith("-")) {
+            ordering = ordering.substring(1);
+            order = SortOrder.DESC;
+        } else {
+            order = SortOrder.ASC;
+        }
+        return new Ordering(ordering, order);
+    }
+
+    @JsonValue
+    String jsonValue();
+
 }
