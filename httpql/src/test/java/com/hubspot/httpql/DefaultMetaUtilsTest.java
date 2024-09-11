e32431ef9ebcb778edc5c61ce68ed983821135d2
skudikala <skudikala@hubspot.com>
2024-09-11 11:44:29 -0400

    forward compatibility with jackson having removed LowerCaseWithUnderscoresStrategy

---
+++ httpql/src/test/java/com/hubspot/httpql/DefaultMetaUtilsTest.java
@@ -0,0 +1,41 @@
+package com.hubspot.httpql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.hubspot.rosetta.annotations.RosettaNaming;
+import org.junit.Test;
+
+public class DefaultMetaUtilsTest {
+
+  @Test
+  public void itCorrectlyDetectsAndConvertsToSnakeCase() {
+    assertThat(DefaultMetaUtils.convertToSnakeCaseIfSupported("aCamelCase", test.class))
+      .isEqualTo("a_camel_case");
+    assertThat(DefaultMetaUtils.convertToSnakeCaseIfSupported("ACamelCase", test1.class))
+      .isEqualTo("a_camel_case");
+    /*
+     * LowerCaseWithUnderscoresStrategy has been removed in later version of jackson, commenting out for compatibility
+     * assertThat(DefaultMetaUtils.convertToSnakeCaseIfSupported("ACamelCase", test2.class))
+     * .isEqualTo("a_camel_case");
+     */
+    assertThat(
+      DefaultMetaUtils.convertToSnakeCaseIfSupported(
+        "ACamelCase",
+        DefaultMetaUtilsTest.class
+      )
+    )
+      .isEqualTo("ACamelCase");
+  }
+
+  @RosettaNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+  public static class test {}
+
+  @RosettaNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+  public static class test1 {}
+  /*
+   * @RosettaNaming(PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy.class)
+   * public static class test2 {}
+   */
+}
